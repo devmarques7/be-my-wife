8957b80605fec26385b775c58112b78243b08092
Daniel Marques <danielmarques@Daniels-MBP.home>
2024-07-03 21:44:41 +0200

    Fix | fixed Url Image

--- src/languages/PT/index.ts
+++ src/languages/PT/index.ts
@@ -52,7 +52,7 @@ export const WEB_SITE_CONTENT_PT = {
     PHOTOS_LOCATION:[
       {SRC:"/source/villa.jpg"},
       {SRC:"/source/villa2.jpg"},
-      {SRC:"/source/villa3.jpg"}
+      {SRC:"/source/villa4.jpg"}
     ]
   },
   FOOTER : {
